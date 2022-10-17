10cd8dadaa527c3224a6a19bccf55fa0b79dd78c
Xiaomi Team <112295130+XiaomiBOT@users.noreply.github.com>
2022-10-17 19:17:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285822347348','6285822347348','6285822347348']
-global.premium = ['6285822347348']
+global.owner = ['6285779150501','6285779150501','6285779150501']
+global.premium = ['6285779150501']
 global.packname = 'Sticker by'
-global.author = 'ArullOfc'
+global.author = 'HanzOfc'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
